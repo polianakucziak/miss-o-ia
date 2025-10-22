e7b9f40b0faba6dcf9e2b9be23fdcacdc8bf1bcf
polianakucziak <140642560+polianakucziak@users.noreply.github.com>
2025-10-22 14:19:49 -0300

    jj

--- java/script.js
+++ java/script.js
@@ -12,3 +12,31 @@ let perguntaAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('click,iniciaJogo');
+function iniciaJogo (){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.computedStyleMap.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta (){
+    if (atual >= perguntas.lenght){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaResultados.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+    }
+}
