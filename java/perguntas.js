f01449b9795f541a43bd88dac3e846cd93c5ac35
polianakucziak <140642560+polianakucziak@users.noreply.github.com>
2025-11-03 09:48:28 -0300

    ggh

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,4 +1,12 @@
 export const perguntas ={
     enunciado:"O Painel Intergovernamental sobre Mudanças Climáticas (IPCC) alertou que o mundo tem um período de tempo limitado (cerca de uma década, a partir de agora) para realizar cortes profundos e rápidos nas emissões de gases de efeito estufa e evitar as consequências mais catastróficas do aquecimento global. A inação ou a ação insuficiente neste período decisivo determinará se as futuras gerações herdarão um planeta relativamente estável ou uma Terra marcada por crises ambientais e sociais contínuas. O que você faria "
-    alternativas 
+    alternativas:[
+        {
+             texto:"A humanidade está em uma encruzilhada climática devido ao aumento das emissões de gases de efeito estufa. As ações que tomamos hoje determinarão a qualidade de vida das futuras gerações.O Desafio Central: Promover uma transformação global rápida e profunda (em energia, consumo e governança) para limitar o aquecimento global a $1.5^\circ\text{C}$ e garantir um futuro habitável."
+             afirmacao:[
+                "Ação coordenada, investimento em energias limpas e economia circular. Um planeta mais estável, limpo e justo, com impactos climáticos gerenciáveis."
+             ]
+     
+            }
+    ]
 }
