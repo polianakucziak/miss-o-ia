6aef7d0b3d3ce9dfba49d603619b008f3a97a054
polianakucziak <140642560+polianakucziak@users.noreply.github.com>
2025-11-05 14:27:14 -0300

    yhyhy

--- java/perguntas.js
+++ java/perguntas.js
@@ -5,7 +5,7 @@ export const perguntas = [
         {
             texto: "Implementar imediatamente um pacote de medidas globais e coordenadas para alcançar a neutralidade de carbono até meados do século, investir maciçamente em energias renováveis, transportes sustentáveis, e promover a restauração de ecossistemas.",
             afirmacao:[
-                "Ação coordenada, investimento em energias limpas e economia circular.  Um planeta mais estável, limpo e justo, com impactos climáticos gerenciáveis.A adoção urgente da neutralidade de carbono é justificada porque o custo da inação (catástrofe futura) é incomensuravelmente maior do que o custo da transição (sacrifício presente). Agir agora é a forma mais eficaz de minimizar o dano total e o sofrimento agregado para todas as gerações."
+                "Ação coordenada, investimento em energias limpas e economia circular.  Um planeta mais estável, limpo e justo, com impactos climáticos gerenciáveis.A adoção urgente da neutralidade de carbono é justificada porque o custo da inação (catástrofe futura) é incomensuravelmente maior do que o custo da transição (sacrifício presente). Agir agora é a forma mais eficaz de minimizar o dano total e o sofrimento agregado para todas as gerações.",
            ],
            proxima : 1,
         },
@@ -15,6 +15,26 @@ export const perguntas = [
                "O melhor resultado alcançado pela Ação Imediata é a garantia de um futuro com clima estável, menos eventos extremos e maior estabilidade de recursos e social. Isso protege a saúde, a segurança e a economia de nossos descendentes, garantindo um planeta muito mais habitável e próspero."
           ],
           proxima: 2,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        "Considerando que as nações desenvolvidas historicamente contribuíram mais para as emissões de gases de efeito estufa, e que as nações em desenvolvimento são frequentemente as mais vulneráveis aos impactos climáticos, qual abordagem de ação climática equilibra a urgência de cortes drásticos de emissões com os princípios de justiça e equidade global, de forma a obter o maior engajamento e o resultado mais benéfico para o conjunto da população mundial?",
+        {
+            texto:"Ação que mais maximiza o bem-estar global: As nações ricas devem liderar os cortes e financiar a transição nos países mais pobres. Isso acelera o corte de emissões globais e garante o engajamento de todos, sendo o caminho mais eficaz para evitar o máximo de sofrimento futuro.",
+            afirmacao:[
+                 "O objetivo é minimizar o sofrimento e o dano total ao longo do tempo. A intervenção agora é justificada porque o custo presente do sacrifício é muito inferior à catástrofe futura evitada. Esta é a via que assegura o maior bem-estar e segurança agregados para a maioria das gerações.",
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima: 4,
         }
     ]
 },
@@ -36,5 +56,43 @@ export const perguntas = [
             proxima: 4,
         }
     ]
-}
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
 ]
